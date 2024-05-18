32c6cb98d3ed284e1c8bed32359e4d8238a3eac7
Vishal <vishalpal1501@gmail.com>
2024-05-18 12:51:41 +0530

    Data tydpes in Python

---
+++ Variables and  Data Types/dataTypes.py
@@ -0,0 +1,13 @@
+# Mainly in python there are five types of data types
+# 1. String
+# 2. Integer
+# 3. Boolean
+# 4. Float
+# 5. None
+
+name="Vishal"   #string
+age=23          #integer
+percentage=34.5 #float
+sdw=True        #Boolean
+
+print(type(percentage))

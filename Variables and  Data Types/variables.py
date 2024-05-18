80367bc32afb93e5ecf027a2524acc9ee3a457d4
Vishal <vishalpal1501@gmail.com>
2024-05-18 12:44:20 +0530

    variables

--- Variables and  Data Types/variables.py
+++ Variables and  Data Types/variables.py
@@ -1,1 +1,7 @@
-print("Hello World")
+name="Vishal Pal"
+age=23
+print(name)
+print(age)
+
+
+# If we are define a variable then we can't start from a digit and we can't use special character like @#$%^&*

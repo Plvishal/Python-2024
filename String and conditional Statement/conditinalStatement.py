853f981482b0328f6e38b36fe48ad3254c6643ba
Vishal <vishalpal1501@gmail.com>
2024-05-19 12:43:43 +0530

    "Added conditional statement to determine student grade based on marks,

--- String and conditional Statement/conditinalStatement.py
+++ String and conditional Statement/conditinalStatement.py
@@ -20,3 +20,16 @@ elif(ligth=="green"):
     print("Now you can go")
 else:
     print("Go careful ")
+
+
+# Provide a Grade student based on marks
+mark=8
+
+if(mark>=90):
+    print("Grade A")
+elif(90>=mark and mark>=80):
+    print("Grade B")
+elif(80>=mark and mark>=70):
+    print("Grade C")
+else:
+    print("Grade D")

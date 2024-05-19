6110e89d55d57a436f77643fb365958a5629f4cc
Vishal <vishalpal1501@gmail.com>
2024-05-19 11:40:21 +0530

    What is string and done some task using the string function

---
+++ String and conditional Statement/string.py
@@ -0,0 +1,22 @@
+# String  is a data types that stores a sequence of character
+# In python there are three way define string and all is valid
+str1="Vishal Pal"
+str2='Lucknow'
+str3='''Uttar Pradesh'''
+print(str1)
+print(str2)
+print(str3)
+
+
+# String concatenation
+print(str1+" "+str2+" "+str3)
+
+# find the length of the any string
+print("Length of the: ", len(str3))    #whitespace is also count one 
+
+# Indexes of string
+print(str1[9])
+
+# Slicing string   Syntax : str[starting_indx : ending_indx]
+print(str2[1:5])
+

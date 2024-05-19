5d0bac021e5dad25173e276af64b05228862e270
Vishal <vishalpal1501@gmail.com>
2024-05-19 12:07:17 +0530

    Conditional statement if-elif-else

---
+++ String and conditional Statement/conditinalStatement.py
@@ -0,0 +1,22 @@
+# Syntax: if-elif-else
+'''
+if(condition):
+    Statement1
+elif(condition):
+    Statement2
+else:
+    Statement N
+
+'''
+
+age=23
+if(age>=18):
+    print("Now You can vote")
+
+ligth="red"
+if(ligth=="red"):
+    print("Now you can wait for ligth green")
+elif(ligth=="green"):
+    print("Now you can go")
+else:
+    print("Go careful ")

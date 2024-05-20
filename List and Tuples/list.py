e57ac6f2ab5b6b0f50fdb593e4c0d8f62415b5f1
Vishal <vishalpal1501@gmail.com>
2024-05-20 13:30:27 +0530

    whta is list and Slicing

---
+++ List and Tuples/list.py
@@ -0,0 +1,10 @@
+# A built-in data typrd that stores set of value
+# List are mutable
+# It can store all types(integers, float, string etc)
+
+marks=[95,54,96.5 ,25 ,"Vishal"]
+print(marks)
+
+print(type(marks))
+# SLicing
+print(marks[2:4])

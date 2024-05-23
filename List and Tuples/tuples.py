b72becbc394ebadc1a14e0e8b23ba163a1ceb245
Vishal <vishalpal1501@gmail.com>
2024-05-23 11:33:31 +0530

    tuples done

---
+++ List and Tuples/tuples.py
@@ -0,0 +1,15 @@
+# A built-in data types that lets us create immutable sequence of values
+# Syntax : tup=()
+# Tuples are ordered and immutable
+# Tuples are defined using parentheses ()
+
+tup=(2,4,5,2,3,2,2)
+t=tup.index(2)  # if find  the element then return the index of the element
+print(t)
+
+c=tup.count(2) # count of toatal occuranace
+print(c)
+
+
+b={"name":"Vishal"}
+print(type(b))

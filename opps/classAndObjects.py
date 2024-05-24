e48a6eb0761d5b7a4efe112c7381b614ed9d9895
Vishal <vishalpal1501@gmail.com>
2024-05-24 13:34:03 +0530

    what is the class and object

---
+++ opps/classAndObjects.py
@@ -0,0 +1,17 @@
+# This is th class
+class Student:
+    name="Vishal"
+
+# This is the object instance of the class
+s1=Student()
+c=s1.name
+print(c)
+
+# Example 2: This  is the  class
+class Car:
+    color="blue"
+    brand="mercedes"
+# This is the object
+c1=Car()
+print(c1.color,end=" ")
+print(c1.brand)
